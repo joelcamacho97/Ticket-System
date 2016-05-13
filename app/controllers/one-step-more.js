bc1eaf63a2e3157358a98bf8a5609072401a7081
joelcamacho97 <joel@outlook.pt>
2016-05-13 18:53:58 +0100

    Correcting some parts of the code in one-step-more.js page.

--- app/controllers/one-step-more.js
+++ app/controllers/one-step-more.js
@@ -8,12 +8,29 @@ export default Ember.Controller.extend({
 
             var uid = this.get('session.uid');
 
-
             console.log(uid);
 
             var login = this.store.createRecord('employers', {
                 'id': this.get('session.uid'),
-                 Name: this.get('userName')
+                Name: this.get('userName'),
+
+                Ability: {
+
+                    "Analise": this.get('analise'),
+                    "Orçamento": false,
+                    "Instalação": false,
+                    "Manutenção": false,
+                    "Software": false,
+                    "Hardware": false,
+                    "Formação": false,
+                    "software": {
+                        "ICG": false,
+                        "Primavera": false,
+                        "SAGE": false
+                    }
+
+                }
+
             });
 
             login.save();
@@ -25,3 +42,21 @@ export default Ember.Controller.extend({
 
 
 });
+
+
+
+/*    Ability: {
+
+                    "Analise": this.get('analise'),
+                    "Orçamento": this.get('orcamento'),
+                    "Instalação": this.get('instalacao'),
+                    "Manutenção": this.get('manutencao'),
+                    "Hardware": this.get('hardware'),
+                    "Formação": this.get('formação'),
+                    "software": {
+                        "ICG": this.get('icg'),
+                        "Primavera": this.get('primavera'),
+                        "SAGE": this.get('sage')
+                    },
+
+                }*/
--- app/models/employers.js
+++ app/models/employers.js
@@ -4,12 +4,11 @@ import attr from 'ember-data/attr';
 export default Model.extend({
 
     Name: attr('string'),
+    Ability: attr(),
     Level: attr('string', {
         defaultValue: "1"
     }),
     Supervisor: attr('boolean', {
         defaultValue: false
     })
-
-
 });
