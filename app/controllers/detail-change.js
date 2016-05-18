205da97b9890d92407a4df523076647c450cbe3d
joelcamacho97 <joel@outlook.pt>
2016-05-18 17:59:44 +0100

    Already it is possible to edit the  name .

--- app/controllers/detail-change.js
+++ app/controllers/detail-change.js
@@ -2,6 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+    actions: {
 
+        editar: function() {
+
+            if (this.get('name') === "") {
+                console.log("Insira aqui alogo -.-")
+
+            } else {
+
+                var fredNameRef = new Firebase("https://brainpcn.firebaseio.com/employers/" + this.get('session.uid'));
+                fredNameRef.child('Name').set(this.get('name'));
+
+            }
+        }
+
+    }
 
 });
