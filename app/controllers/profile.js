abbf199e78c5d3e59acaf98248ca015fb416e83d
joelcamacho97 <joel@outlook.pt>
2016-05-18 16:46:29 +0100

    Corrections in the profile page code

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -2,6 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+    actions: {
 
+        apagar: function() {
+
+            var ref = new Firebase("https://brainpcn.firebaseio.com/");
+            var _this = this;
+
+
+            ref.removeUser({
+                email: this.get('email'),
+                password: this.get('password')
+            }, function(error) {
+                if (error === null) {
+                    console.log("User removed successfully");
+                } else {
+                    console.log("Error removing user:", error);
+                }
+            });
+
+        }
+    }
 
 });
--- app/routes/detail-change.js
+++ app/routes/detail-change.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+
+    model: function() {
+
+        if (!this.get('session.isAuthenticated')) {
+            this.transitionTo('login');
+        }
+        return this.store.find('employers', this.get('session.uid'));
+    }
+
+
+
 });
