ef517c667cab880f6b49bb5f270e9d73953024c0
joelcamacho97 <joel@outlook.pt>
2016-05-18 14:59:48 +0100

    Already it is possible to edit the Password

---
+++ app/controllers/password-change.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+    actions: {
+
+        editar: function() {
+
+            var ref = new Firebase("https://brainpcn.firebaseio.com/");
+            var _this = this;
+
+
+            if (this.get('RepitaPassword') === this.get('NovaPassword')) {
+
+                ref.changePassword({
+                    email: this.get('session.currentUser.email'),
+                    oldPassword: this.get('PasswordAtual'),
+                    newPassword: this.get('NovaPassword')
+                }, function(error) {
+                    if (error === null) {
+                        _this.transitionToRoute('profile'); //ou outro preocesso de next para o preocesso qd concluido ...
+                    } else {
+                        console.log("Erro:", error);
+                    }
+                });
+
+            } else {
+
+                console.log("Nao corresponde ....");
+
+            }
+
+
+        }
+    }
+
+});
