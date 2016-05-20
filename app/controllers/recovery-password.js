a7a3c12856a53f4ed79c5af507093d586cf71149
joelcamacho97 <joel@outlook.pt>
2016-05-20 17:15:43 +0100

    Added error notification in recovery password

--- app/controllers/recovery-password.js
+++ app/controllers/recovery-password.js
@@ -1,23 +1,33 @@
 import Ember from 'ember';
-
 export default Ember.Controller.extend({
 
+    tag: null,
 
     actions: {
 
+
         recuperar: function() {
 
+            //  var controller = Ember.Object.create();
+            const truee = this.set('tag', true);
+
             var ref = new Firebase("https://brainpcn.firebaseio.com/");
-            var _this = this;
 
             ref.resetPassword({
                 email: this.get('email')
             }, function(error) {
                 if (error === null) {
+
                     console.log("Password reset email sent successfully");
+                    //     return this.set('tag', false);
                 } else {
                     console.log("Error sending password reset email:", error);
+                    truee
+                    //this.set('tag', false);
+
+
                 }
+
             });
 
         }
