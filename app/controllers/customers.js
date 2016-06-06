229ed4b571241a9b85f2ca3f00202699b844c516
joelcamacho97 <joel@outlook.pt>
2016-06-06 16:45:01 +0100

    Fix problems in table. Necessary to improve table performance

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -2,36 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    sortProperties: ['id'],
-    theFilter: "",
-
-    checkFilterMatch: function(theObject, str) {
-        var field, match;
-        match = false;
-        for (field in theObject) {
-            if (theObject[field].toString().slice(0, str.length) === str) {
-                match = true;
-            }
-        }
-        return match;
-    },
-
-    filterPeople: (function() {
-        return this.get("arrangedContent").filter((function(_this) {
-            return function(theObject, index, enumerable) {
-                if (_this.get("theFilter")) {
-                    return _this.checkFilterMatch(theObject, _this.get("theFilter"));
-                } else {
-                    return true;
-                }
-            };
-        })(this));
-    }).property("theFilter", "sortProperties"),
-
-    actions: {
-        sortBy: function(property) {
-            this.set("sortProperties", [property]);
-        }
-    }
-
 });
