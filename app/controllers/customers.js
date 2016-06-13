3bc8d66283ac7f43b4fd944e1cb4c6db7303a1b3
joelcamacho97 <joel@outlook.pt>
2016-06-13 18:59:20 +0100

    Paging creation completed and running

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
 
-
-
 export default Ember.Controller.extend({
+
+    i: 0,
+
+    actions: {
+
+        mais: function(multi) {
+
+            multi = this.incrementProperty('i') * 5;
+
+            console.log(multi);
+
+            this.get('model').set('offset', multi);
+        },
+        menos: function(multi) {
+
+            multi = this.decrementProperty('i') * 5;
+            this.get('model').set('offset', multi);
+
+            if (multi < 0) {
+                this.incrementProperty('i') * 0;
+                this.get('model').set('offset', multi);
+            }
+
+        }
+
+    }
 
 });
--- app/routes/customers.js
+++ app/routes/customers.js
@@ -1,11 +1,30 @@
 import Ember from 'ember';
-
-
+//import ArraySlice from 'array-slice';
+import ArrayPager from 'array-pager';
 
 export default Ember.Route.extend({
 
     model() {
-        return this.store.findAll('customers');
+
+
+        var array = this.store.findAll('customers');
+
+        var tabela = ArrayPager.create({
+            content: array,
+            limit: 5
+        });
+
+        // console.log(array.toArray());
+        //  i = i + 5
+        // console.log(i);
+        // tabela.set('offset', i);
+
+        // console.log(this.get('model') + " ola");
+
+        return tabela;
     }
+
+
+
 
 });
